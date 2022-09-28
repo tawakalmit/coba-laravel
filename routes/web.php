3cb1861e2e0dc38fa7d5119d5bd010b6b5b69281
tawakalmit <tawakal.mit@gmail.com>
2022-09-28 17:28:40 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/profile', function () {
+    $name = "Ikbal";
+    return view('profile', [
+        "name" => $name
+    ]);
 });
